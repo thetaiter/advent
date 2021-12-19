898b1cb2ff2136945018a3508af909598f13d68d
Ian Tait <thetaiter@gmail.com>
2021-12-18 19:33:08 -0600

    Day 2

---
+++ 2021/day2/part2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+# Read lines of input file into an array
+lines = []
+with open(os.path.join(sys.path[0], 'input.txt'), 'r') as input:
+    lines = input.read().splitlines()
+
+horizontal_position = 0
+depth = 0
+aim = 0
+
+commands = {
+    'forward': lambda x: globals().update(
+        horizontal_position = horizontal_position + x,
+        depth = depth + (aim * x)
+    ),
+    'up': lambda x: globals().update(aim = aim - x),
+    'down': lambda x: globals().update(aim = aim + x)
+}
+
+for command in lines:
+    command_array = command.split(' ')
+    direction = command_array[0]
+    amount = int(command_array[1])
+    commands[direction](amount)
+
+print(horizontal_position * depth)
