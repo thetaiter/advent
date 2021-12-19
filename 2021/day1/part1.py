8038472a42f1aeeefc2a13c2ae8299ee791d514d
Ian Tait <thetaiter@gmail.com>
2021-12-18 20:26:07 -0600

    Time different methods in day1

--- 2021/day1/part1.py
+++ 2021/day1/part1.py
@@ -2,14 +2,18 @@
 
 import os
 import sys
+import time
 
 # Read lines of input file into an array
 lines = []
 with open(os.path.join(sys.path[0], 'input.txt'), 'r') as input:
    lines = input.read().splitlines()
 
-# For Loop Method
 timesDepthIncreased = 0
+
+# For Loop Method
+start = time.time()
+
 for i, line in enumerate(lines):
     if i > 0:
         previousDepth = int(lines[i-1])
@@ -18,10 +22,17 @@ for i, line in enumerate(lines):
         if currentDepth > previousDepth:
             timesDepthIncreased += 1
 
-print(timesDepthIncreased)
+end = time.time()
+
+print('For Loop Method:', timesDepthIncreased)
+print('Time to calculate:', end - start, 'seconds')
 
 # Zip + List Comprehension Method
+start = time.time()
 increasedArray = [int(j) > int(i) for i, j in zip(lines, lines[1:])]
 timesDepthIncreased = len([item for item in increasedArray if item == True])
+end = time.time()
 
-print(timesDepthIncreased)
+print()
+print('Zip + List Comprehension Method:', timesDepthIncreased)
+print('Time to calculate:', end - start, 'seconds')
