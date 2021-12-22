46492332b960dee7d388c22c8dc4b6c45983e359
Ian Tait <thetaiter@gmail.com>
2021-12-22 17:49:41 -0600

    Improved new.py argument parsing/validation, added description/usage

--- new.py
+++ new.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python
 
 import sys
+from datetime import date
 from helpers import *
 
-# Grab arguments from commandline
-try:
-    year = int(sys.argv[1])
-except:
-    year = 2015
+description = 'Generate the directories and files for a new Advent of Code problem'
+usage = f'''Usage: {sys.argv[0]} [year] [day]
 
-try:
-    day = int(sys.argv[2])
-except:
-    day = 1
+[year] Must be a 4 digit integer between 2015 and the current year (inclusive), default is 2015
+[day] Must be an integer between 1 and 25, default is 1
+
+Note: If [year] is the current year, [day] cannot be a day in December that has not yet come to pass'''
+
+# Parse commandline arguments
+if any(h in sys.argv for h in ['help', '-help', '--help', '-h']):
+    print(f'{description}\n')
+    print(usage)
+    exit()
+
+problem_date = [2015, 1]
+
+for index, arg in enumerate(sys.argv[1:]):
+    try:
+        problem_date[index] = int(arg)
+    except:
+        print(f'Invalid argument: {arg}', file=sys.stderr)
+        print(usage, file=sys.stderr)
+        exit(1)
+
+year = problem_date[0]
+day = problem_date[1]
+
+if not (2015 <= year <= date.today().year):
+    print(f'Invalid year: {year}', file=sys.stderr)
+    print(usage, file=sys.stderr)
+    exit(1)
+
+if not (1 <= day <= (date.today().day if year == date.today().year else 25)):
+    print(f'Invalid day: {day}', file=sys.stderr)
+    print(usage, file=sys.stderr)
+    exit(1)
 
 # Write problem file with first part of the problem (2nd part requires authentication)
 problem = getProblem(year, day)
 problem_dir = f'{year}/day{day}'
 writeFile(f'{problem_dir}/README.md', problem)
 
-# Write part1.py from template
+# Write part1.py and part2.py from template
 part_template = readFile('helpers/template.py', return_type=str)
 part1_file = f'{problem_dir}/part1.py'
 part2_file = f'{problem_dir}/part2.py'
 writeFile(part1_file, part_template, executable=True)
 writeFile(part2_file, part_template, executable=True)
 
-# Touch input.txt file (cannot get it automatically... yet. It requires authentication)
+# Touch input.txt file (cannot get content automatically yet, it requires authentication)
 touchFile(f'{problem_dir}/input.txt')
